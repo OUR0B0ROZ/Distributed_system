015ba3b2a6380c197d92a5c474a0f2b48a85d916
OUR0B0ROZ <jacobo.favela.irt@unipolidgo.edu.mx>
2023-09-24 18:58:19 -0700

    P2_core_I

--- distributed_system/web_server/web_server/settings.py
+++ distributed_system/web_server/web_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wjl^8)xwy^zr1%7rq6ss6ot=-wd&jfrl63gtnc03s))d@-2ox5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
