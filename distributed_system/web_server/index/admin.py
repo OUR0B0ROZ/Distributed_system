bd04ff3073b83c86d02e945fc2837a4ef7082f7e
OUR0B0ROZ <jacobo.favela.irt@unipolidgo.edu.mx>
2023-10-01 18:11:44 -0700

    P1_core_MSR

--- distributed_system/web_server/index/admin.py
+++ distributed_system/web_server/index/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Category, ModelTest
 # Register your models here.
+@admin.register(Category)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(ModelTest)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'created']
+    list_filter = ['created', 'category']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
--- distributed_system/web_server/index/models.py
+++ distributed_system/web_server/index/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    
+    class Meta:
+        ordering = ['title']
+    def __str__(self):
+        return self.title
+
+class ModelTest(models.Model):
+    autor = models.ForeignKey(User,
+        related_name='models_created',
+        on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,
+        related_name='models',
+        on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created']
+    def __str__(self):
+        return self.title
