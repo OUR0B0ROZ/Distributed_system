fca6a05bb50f676897544e35f6cc116afa1e4512
OUR0B0ROZ <jacobo.favela.irt@unipolidgo.edu.mx>
2023-10-04 20:50:15 -0700

    P1_core_MSR

--- distributed_system/web_server/index/urls.py
+++ distributed_system/web_server/index/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView,ModelDetailView
 
 app_name = 'index'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+     path('<slug:slug>/', ModelDetailView.as_view(), name='model-detail'),
 ]
--- distributed_system/web_server/index/views.py
+++ distributed_system/web_server/index/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import ModelTest
+from django.views.generic import DetailView
 # Create your views here.
 
 
-class IndexView(TemplateView):
-    template_name = 'index/index.html'
+#class IndexView(TemplateView):
+#   template_name = 'index/index.html'
+
+class IndexView(ListView):
+    model = ModelTest  # Set the model to 'ModelTest'
+    template_name = 'index/model_list.html'  # Specify the template for the list view
+    context_object_name = 'index.html'  # Name of the variable to use in the template
+    
+class ModelDetailView(DetailView):
+    model = ModelTest  # Set the model to 'ModelTest'
+    template_name = 'index/model_detail.html'  # Specify the template for the detail view
+    context_object_name = 'model'  # Name of the variable to use in the template
