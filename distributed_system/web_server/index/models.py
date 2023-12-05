58066d106f8e4a678f9c9c8998b4d20f527e6a9e
OUR0B0ROZ <jacobo.favela.irt@unipolidgo.edu.mx>
2023-12-04 20:03:38 -0700

    P1_core_CS

--- distributed_system/web_server/index/models.py
+++ distributed_system/web_server/index/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 # Create your models here.
 
 class Category(models.Model):
@@ -11,18 +12,34 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+
+def generate_slug(title):
+    return slugify(title)
+
 class ModelTest(models.Model):
     autor = models.ForeignKey(User,
-        related_name='models_created',
-        on_delete=models.CASCADE)
+                              related_name='models_created',
+                              on_delete=models.CASCADE)
     category = models.ForeignKey(Category,
-        related_name='models',
-        on_delete=models.CASCADE)
+                                 related_name='models',
+                                 on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    creation_year = models.PositiveIntegerField()  # Field for storing the year of creation
+    initial = models.CharField(max_length=1)  # Field for storing the first letter of the title
+
     class Meta:
-        ordering = ['-created']
+        ordering = ['initial', '-creation_year', 'title']
+
+    def save(self, *args, **kwargs):
+        # Generate slug, set creation_year, and initial before saving
+        if not self.slug:
+            self.slug = generate_slug(self.title)
+        self.creation_year = self.created.year
+        self.initial = self.title[0].upper()  # Store the first letter (capitalized)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
