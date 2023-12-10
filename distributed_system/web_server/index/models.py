75fdc93907c999e95d8a6a22916e1fc003c5b885
OUR0B0ROZ <jacobo.favela.irt@unipolidgo.edu.mx>
2023-12-09 20:14:07 -0700

    P1_core_CS

--- distributed_system/web_server/index/models.py
+++ distributed_system/web_server/index/models.py
@@ -17,6 +17,15 @@ def generate_slug(title):
     return slugify(title)
 
 class ModelTest(models.Model):
+
+    APPROVED = 'approved'
+    UNDER_REVIEW = 'under_review'
+
+    STATUS_CHOICES = [
+        (APPROVED, 'Approved'),
+        (UNDER_REVIEW, 'Under Review'),
+    ]
+
     autor = models.ForeignKey(User,
                               related_name='models_created',
                               on_delete=models.CASCADE)
@@ -28,7 +37,11 @@ class ModelTest(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     creation_year = models.PositiveIntegerField()  # Field for storing the year of creation
-    initial = models.CharField(max_length=1)  # Field for storing the first letter of the title
+    initial = models.CharField(max_length=1, blank=True)  # Field for storing the first letter of the title
+
+    status = models.CharField(max_length=20,
+                              choices=STATUS_CHOICES,
+                              default=UNDER_REVIEW)  # Default is set to 'Under Review'
 
     class Meta:
         ordering = ['initial', '-creation_year', 'title']
@@ -37,7 +50,6 @@ class ModelTest(models.Model):
         # Generate slug, set creation_year, and initial before saving
         if not self.slug:
             self.slug = generate_slug(self.title)
-        self.creation_year = self.created.year
         self.initial = self.title[0].upper()  # Store the first letter (capitalized)
         super().save(*args, **kwargs)
 
--- distributed_system/web_server/index/views.py
+++ distributed_system/web_server/index/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import ListView
-from .models import ModelTest
+from .models import ModelTest,Category
 from django.views.generic import DetailView
 # Create your views here.
 
@@ -14,7 +14,38 @@ class ModelListView(ListView):
     template_name = 'index/model_list.html'  # Specify the template for the list view
     context_object_name = 'model_list'  # Name of the variable to use in the template
     
+    def get_queryset(self):
+        sort_option = self.request.GET.get('sort_option', 'numerical')
+        category_filter = self.request.GET.get('category_filter')
+
+        queryset = ModelTest.objects.all()
+
+        if category_filter:
+            queryset = queryset.filter(category__slug=category_filter)
+
+        if sort_option == 'numerical':
+            queryset = sorted(queryset, key=lambda x: (x.creation_year, x.initial, x.title))
+        elif sort_option == 'alphabetical':
+            queryset = sorted(queryset, key=lambda x: (x.initial, x.title, x.creation_year))
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sort_option'] = self.request.GET.get('sort_option', 'numerical')
+        context['category_filter'] = self.request.GET.get('category_filter')
+        context['categories'] = Category.objects.all()
+
+          # Set the selected category
+        context['selected_category'] = (
+            Category.objects.get(slug=context['category_filter'])
+            if context['category_filter']
+            else None
+        )
+        return context
+
 class ModelDetailView(DetailView):
     model = ModelTest  # Set the model to 'ModelTest'
     template_name = 'index/model_detail.html'  # Specify the template for the detail view
     context_object_name = 'model'  # Name of the variable to use in the template
+
